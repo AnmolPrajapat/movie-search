9dbe24713bf9bd5032f6f9792a1efccbc7774451
AnmolPrajapat <prajapatanmol2001@gmail.com>
2021-03-08 23:19:52 +0530

    updated searchmovie and css

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,96 +1,83 @@
 import React, { useState } from "react";
 import FormComponent from "./FormComponent";
-
 export default function SearchMovies() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [apiCalled, setApiCall] = useState(false);
+  const [apiCalled, setApicall] = useState(false);
 
-  const searchMovies = async (e) => {
+  async function SearchMovies(e) {
     e.preventDefault();
 
     const url = `https://api.themoviedb.org/3/search/movie?api_key=65b2d4a08fc4519f762f7531fe791385&language=en-US&query=${query}&page=1&include_adult=false`;
 
     try {
-        if(validateForm(query)) {
-            const res = await fetch(url);
-            const data  = await res.json();
-            setMovies(data.results);
-            setApiCall(true);
-            console.log(data)
-        }
-    }
-    catch(err){
-        console.error(err);
+      if (validateForm(query)) {
+        const res = await fetch(url);
+        const data = await res.json();
+        setMovies(data.results);
+        setApicall(true);
+      }
+    } catch (err) {
+      console.error(err);
     }
   }
-  function showList() {
-    if(movies.length) {
-        return (
-            <div>
-                <div className="card-list">
-                {
-                    movies.filter(movie => movie.poster_path)
-                        .map(movie => (
-                            <FormComponent movie={movie} key={movie.id} />
-                        )
-                    )
-                }
-                </div>
-                <br/><br/><br/>
-                <footer className = "foot" >Thank you for visiting!!</footer>
-            </div>
-            
-            
-        )
-    } else if(!movies.length && apiCalled){
-        return (
-            <div>
-                <div className="notFound" >Sorry,there is no movies as {query}</div>
-                <br/><br/><br/>
-                <footer className = "foot" >Thank you for visiting!!</footer>
-            </div>
-        )
-    }
-}
-
-
-  return (
-    <div>
-      <form action="" className="form" onSubmit={searchMovies}>
-        <label htmlFor="query" className="label">
-          movie name{" "}
-        </label>
-        <input
-          type="text"
-          className="input"
-          name="query"
-          placeholder="type your search"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        required/>
-
-        <button className="button " type="submit">
-          submit
-        </button>
-      </form>
-      {showList()}
-      </div>
-      
-  )
-}
-
-function validateForm(query) {
+  function validateForm(query) {
     if (query === "") {
-      alert("Please check your input again")
+      alert("Please check your input again");
       return false;
     }
     return true;
+  }
+
+  return (
+    <div>
+      <form onSubmit={SearchMovies}>
+        <label htmlFor="label" className="label">
+          {" "}
+          Movie Name
+        </label>
+        <input
+          type="text"
+          name="query"
+          value={query}
+          placeholder="serach your movies"
+          onChange={(event) => setQuery(event.target.value)}
+          required
+        />
+        <button className="button"> submit</button>
+      </form>
+
+      {showlist()}
+    </div>
+  );
+  function showlist() {
+    if (movies.length) {
+      return (
+        <div>
+          <div className="card-list">
+            {movies
+              .filter((movie) => movie.poster_path)
+              .map((movie) => (
+                <FormComponent movie={movie} key={movie.id} />
+              ))}
+          </div>
+          <br />
+          <br />
+          <br />
+          <footer className="foot">Thank you for visiting!!</footer>
+        </div>
+      );
+    } else if (!movies.length && apiCalled) {
+      return (
+        <div>
+          <div className="not-found">
+            <h1>movie {query} you are searching for is not found in data </h1>
+          </div>
+          <div className="foot">
+            <h1>thank you for searching!</h1>
+          </div>
+        </div>
+      );
+    }
+  }
 }
-
-  
-  
-
-
-
-// 65b2d4a08fc4519f762f7531fe791385
